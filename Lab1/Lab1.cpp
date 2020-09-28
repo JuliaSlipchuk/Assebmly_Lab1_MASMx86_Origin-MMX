528c793d36a1eb65a6edbb1a8dd3096bcff256e4
JuliaSlipchuk <juliaolex@knu.ua>
2020-09-28 09:45:03 +0300

    rename

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,4 +1,4 @@
-﻿// Gavno.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// Lab1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
